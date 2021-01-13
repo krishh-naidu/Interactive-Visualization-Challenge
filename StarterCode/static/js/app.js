e1a347e3040655e630098f240e887490f1d69f93
krishh-naidu <krishh.naidu@gmail.com>
2021-01-13 14:56:10 -0600

    gauge chart is ready

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -34,12 +34,16 @@ function getMetaData(id){
 
 function getPlotforRequestedID(id){
     d3.json('samples.json').then((data)=>{
-        console.log(data);
+        //console.log(data);
+        
+        // capture wfreq for selected ID
+        var metadata=data.metadata.filter(meta=>meta.id.toString() === id)[0];
+        var wfreq = metadata.wfreq;
+        console.log(wfreq);
         
         // filter by ID
         var samples_id=data.samples.filter(s=>s.id.toString() === id)[0];
-        //console.log(samples_id.id);
-
+   
         //get top 10 sample_values for each filtered ID
         top10sample_values = samples_id.sample_values.slice(0,10).reverse();
         console.log('Top 10 Sample IDs:' + top10sample_values);
@@ -118,9 +122,44 @@ function getPlotforRequestedID(id){
           displaylogo:false, 
           responsive:true,
         }
+
+        // Prepare Gauge Chart
+        console.log('Washing Freq:' + wfreq);
+        var data2 = [
+          {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wfreq,
+            title: { text: "Weekly Washing Frequency" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: { axis: { range: [null, 9] },
+                    bar:{color:'red'},
+                   steps: [
+                    { range: [0, 1], color: "yellow" },
+                    { range: [1, 2], color: "cyan" },
+                    { range: [2, 3], color: "teal" },
+                    { range: [3, 4], color: "lime" },
+                    { range: [4, 5], color: "green" },
+                    { range: [5, 6], color: "blue" },
+                    { range: [6, 7], color: "teal" },
+                    { range: [7, 8], color: "gray" },
+                    { range: [8, 9], color: "red" },
+                  ]}
+          }
+        ];
+        
+        var layout2 = { 
+          width: 600, 
+          eight: 500, 
+          margin: { t: 20, b: 40, l:100, r:100},
+          paper_bgcolor: "lavender",
+          font: { color: "darkblue", family: "Arial" }
+        };
+        
         // using react function instead newPlot for better performance
         Plotly.react("bar",data,layout,config);
-        Plotly.react('bubble',data1,layout1,config)
+        Plotly.react('bubble',data1,layout1,config);
+        Plotly.react('gauge', data2, layout2);
 
 
     })
